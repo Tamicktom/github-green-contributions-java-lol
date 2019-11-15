1eef658fbdf30e08c7f86899e84f9973ba47cb1d
Tamicktom <rickpokemon38@gmail.com>
2019-11-15 10:00:39 -0300

    ✨ Java: Atividade #3 em 15/11/2019

---
+++ activities/2019/2019-11-15_activity_2.java
@@ -0,0 +1,30 @@
+/**
+ * Implementando features ✨
+ * Data: 15/11/2019
+ * Commit #2
+ */
+
+public class DailyActivity {
+
+    private String[] activities = {
+        "Implementando padrões de projeto",
+        "Corrigindo exceptions e logs",
+        "Otimizando queries JPA/Hibernate",
+        "Escrevendo testes JUnit",
+        "Refatorando código Spring Boot",
+        "Implementando APIs RESTful"
+    };
+
+    private String currentActivity = "Implementando features ✨";
+    private String date = "15/11/2019";
+
+    public void executeActivity() {
+        System.out.println("Data: " + date);
+        System.out.println("Atividade: " + currentActivity);
+    }
+
+    public static void main(String[] args) {
+        DailyActivity activity = new DailyActivity();
+        activity.executeActivity();
+    }
+}
