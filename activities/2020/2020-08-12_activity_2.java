162af0517bf55f5d721a129baf5b216082b3460f
Tamicktom <rickpokemon38@gmail.com>
2020-08-12 16:55:10 -0300

    ✨ Java: Atividade #3 em 12/08/2020

---
+++ activities/2020/2020-08-12_activity_2.java
@@ -0,0 +1,30 @@
+/**
+ * Otimizando performance ⚡
+ * Data: 12/08/2020
+ * Commit #2
+ */
+
+public class DailyActivity {
+
+    private String[] activities = {
+        "Implementando padrões de projeto",
+        "Corrigindo exceptions e logs",
+        "Otimizando queries JPA/Hibernate",
+        "Escrevendo testes JUnit",
+        "Refatorando código Spring Boot",
+        "Implementando APIs RESTful"
+    };
+
+    private String currentActivity = "Otimizando performance ⚡";
+    private String date = "12/08/2020";
+
+    public void executeActivity() {
+        System.out.println("Data: " + date);
+        System.out.println("Atividade: " + currentActivity);
+    }
+
+    public static void main(String[] args) {
+        DailyActivity activity = new DailyActivity();
+        activity.executeActivity();
+    }
+}
