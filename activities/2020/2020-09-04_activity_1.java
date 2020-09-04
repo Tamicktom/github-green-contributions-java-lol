29c71cce3cb127a90709fcbf27ecebbfcdcb7b24
Tamicktom <rickpokemon38@gmail.com>
2020-09-04 15:31:20 -0300

    ✨ Java: Atividade #2 em 04/09/2020

---
+++ activities/2020/2020-09-04_activity_1.java
@@ -0,0 +1,30 @@
+/**
+ * Resolvendo bugs 🐛
+ * Data: 04/09/2020
+ * Commit #1
+ */
+
+public class DailyActivity {
+
+    private String[] activities = {
+        "Implementando padrões de projeto",
+        "Corrigindo exceptions e logs",
+        "Otimizando queries JPA/Hibernate",
+        "Escrevendo testes JUnit",
+        "Refatorando código Spring Boot",
+        "Implementando APIs RESTful"
+    };
+
+    private String currentActivity = "Resolvendo bugs 🐛";
+    private String date = "04/09/2020";
+
+    public void executeActivity() {
+        System.out.println("Data: " + date);
+        System.out.println("Atividade: " + currentActivity);
+    }
+
+    public static void main(String[] args) {
+        DailyActivity activity = new DailyActivity();
+        activity.executeActivity();
+    }
+}
