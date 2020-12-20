ea79f05abc76e6645a22fa7cfb6f1bf65a9bb33b
Tamicktom <rickpokemon38@gmail.com>
2020-12-20 12:25:21 -0300

    ✨ Java: Atividade #5 em 20/12/2020

---
+++ activities/2020/2020-12-20_activity_4.java
@@ -0,0 +1,30 @@
+/**
+ * Otimizando performance ⚡
+ * Data: 20/12/2020
+ * Commit #4
+ */
+
+public class DailyActivity {
+
+    private String[] activities = {
+        "Implementando padrões de projeto",
+        "Corrigindo exceptions e logs",
+        "Otimizando queries JPA/Hibernate",
+        "Escrevendo testes JUnit",
+        "Refatorando código Spring Boot",
+        "Implementando APIs RESTful"
+    };
+
+    private String currentActivity = "Otimizando performance ⚡";
+    private String date = "20/12/2020";
+
+    public void executeActivity() {
+        System.out.println("Data: " + date);
+        System.out.println("Atividade: " + currentActivity);
+    }
+
+    public static void main(String[] args) {
+        DailyActivity activity = new DailyActivity();
+        activity.executeActivity();
+    }
+}
